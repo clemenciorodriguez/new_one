616638d1d7e7beebc11b8e6b45d6c60f025944da
TOTOGIJUBTS <tigolo4305@rxcay.com>
2023-02-07 20:06:53 -0300

    confirmated

--- index.js
+++ index.js
@@ -1,52 +1,107 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const cors = require('./cors');
- 
+// Importar las dependencias necesarias
+const cors = require("./cors")
+const express = require("express");
+const multer = require("multer");
+const mysql = require("./mysql2");
 
-const app = express();
-const port = process.env.PORT || 3000;
+// Inicializar Express
+
+const app = express()
+
+
 app.use(cors());
 
-// data u:Johns p:qKLUqWfs7PjZwUu2
-//mongodb+srv://Johns:qKLUqWfs7PjZwUu2@cluster0.i2smq6y.mongodb.net/?retryWrites=true&w=majority
-app.use("/api", require("./routes/tracks"))
+// Iniciar el servidor
+app.listen(3000, () => {
+  console.log("Servidor iniciado en el puerto 3000");
+});
 
 
-app.use('/static', express.static(path.join(__dirname, 'static')))
+
+// Configurar la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: 'bzc2vs79rtlg1pmcnuf2-mysql.services.clever-cloud.com',
+  user: 'uftekw6lr9o2a7oq',
+  password:"kfrdejJwCGdj3j8IcrsO",
+  database: 'bzc2vs79rtlg1pmcnuf2'
+});
+
+connection.connect(() => {
+  
+  console.log('Conectado como ID: ' + connection.threadId);
+});
 
 
-// Configuración de Multer para almacenar las imágenes en una carpeta estática
-products = []
+
+
+// Configurar Multer para la subida de archivos
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, 'static/photos/');
+    cb(null, "C:/Nodejs65/static/photos");
   },
   filename: function(req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    cb(null, file.fieldname + "-" + Date.now());
   }
 });
 const upload = multer({ storage: storage });
 
-app.post('/products', upload.array('photos', 50), (req, res) => {
-  const saved_images = req.files.map(file => 'http://' + req.get("host") + '/static/photos/' + file.filename)
-  const product = {
-    name: req.body.name,
-    email: req.body.email,
-    details: req.body.details,
-    lat: req.body.lat,
-    lon: req.body.lon,
-    photos: saved_images
-  };
-  products.push(product);
-  res.status(201).json(product);
+
+
+// Definir el endpoint para procesar el formulario
+app.post("/form", upload.array("image", 12), (req, res) => {
+  const { name, email, details, latitude, longitude } = req.body;
+
+  // Preparar la información de las imágenes para almacenarlas en la base de datos
+  const image_url = req.files.map(file => "C:/Nodejs65/static/photos/" + file.filename);
+  
+
+  // Crear una consulta SQL para insertar los datos en la tabla
+  const sql =
+    "INSERT INTO users (name, email, details, latitude, longitude, image_url) VALUES (?, ?, ?, ?, ?, ?)";
+  const values = [name, email, details, latitude, longitude, JSON.stringify(image_url)];
+
+  // Ejecutar la consulta
+  connection.query(sql, values, (error, results) => {
+    if (error) {
+      res.status(500).send({ error: "Error al procesar el formulario" });
+    } else {
+      res.send({ message: "Formulario procesado correctamente" });
+    }
+  });
 });
 
-app.get('/products', (req, res) => {
-  console.log({ products})
-  res.json({ products})
-})
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}` );
+app.get("/users", (req, res) => {
+  const sql = "SELECT * FROM users";
+  connection.query(sql, (error, results) => {
+    if (error) {
+      res.status(500).send({ error: "Error al obtener datos" });
+    } else {
+      const data = results.map(row => {
+        return {
+          name: row.name,
+          email: row.email,
+          details: row.details,
+          latitude: row.latitude,
+          longitude: row.longitude,
+          imageUrls: JSON.parse(row.image_url)
+        };
+      });
+      res.send({ data });
+    }
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
